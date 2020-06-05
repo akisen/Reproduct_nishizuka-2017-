aef8f15de6d49baa006683dae96e47a83096e423
Akito <akitrb001@gmail.com>
2020-06-05 13:25:49 +0900

    convert_dataset_into_csv.py can convert both sharp and cgem.lorentz data  into sigle csv file

--- convert_dataseta_into_csv.py
+++ convert_dataseta_into_csv.py
@@ -11,26 +11,31 @@ import glob
 import sys
 import pandas as pd
 import os
-def reading_sharp(SHARP_Path):
-    keys = ["t_rec","totusjh","totusjz","absnjzh","savncpp","usflux","area_acr","meangam","meangbt","meangbz","meangbh","meanjzh","meanjzd"]
+def reading_dataset(SHARP_Path,CGEM_Path):
+    sharp_keys = ["t_rec","totusjh","totusjz","absnjzh","savncpp","usflux","area_acr","meangam","meangbt","meangbz","meangbh","meanjzh","meanjzd"]
+    cgem_keys = ["totbsq","totfz","epsz","totfy","totfx","epsy","epsx"]
     sharp_path_obj = sorted(glob.glob(SHARP_Path))
-    sharp_datas =pd.DataFrame([keys])
-    for index,data in enumerate(sharp_path_obj):
-        map =sunpy.map.Map(data)
+    cgem_path_obj =sorted(glob.glob(CGEM_Path))
+    datas =pd.DataFrame([sharp_keys+cgem_keys])
+    
+    for sharp_data,cgem_data in zip(sharp_path_obj,cgem_path_obj):
+        sharp_map =sunpy.map.Map(sharp_data)
+        cgem_map =sunpy.map.Map(cgem_data)
         row=[]
-        for key in keys:
+        for key in sharp_keys:
             # print(str(key)+str(map.meta[key]))
-
-            row.append(str(map.meta[key]))
-        sharp_datas=sharp_datas.append([row])
-    return sharp_datas
+            row.append(str(sharp_map.meta[key]))
+        for key in cgem_keys:
+            row.append(str(cgem_map.meta[key]))
+        datas=datas.append([row])
+    return datas
 
 
 def main():
     args = sys.argv
     SHARP_Path = args[1]
-    # CGEM_Path = args[2]
-    CSV_Path = args[2]
-    reading_sharp(SHARP_Path).to_csv(CSV_Path) 
+    CGEM_Path = args[2]
+    CSV_Path = args[3]
+    reading_dataset(SHARP_Path,CGEM_Path).to_csv(CSV_Path) 
 if __name__ == "__main__":
     main()
