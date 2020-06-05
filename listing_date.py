c69f3405e5fa4d3c4fc7cbd31bb9bbbfde7bf81c
Akito <akitrb001@gmail.com>
2020-06-05 15:04:41 +0900

    Making progress bar to check progression of convert

--- listing_date.py
+++ listing_date.py
@@ -2,7 +2,7 @@
 fitsファイルの中の撮影日を出力するスクリプト。
 データセットをNASに上げる前に漏れがないか確認するようにする
 ex)python3 listing_date.py "/media/akito/Data/Dataset/SHARP(CEA)/2010*/*000000*Bp.fits" >2010.txt
-注意:活動が余り活発な時期でないときには途中のデータが抜けている場合もある→普通に期間で取り出していれば最初と最後を確認すれば大丈夫
+注意:活動が余り活発な時期でないときには途中のデータが抜けている場合もある→普通に期間で取り出していれば
 """
 import sys
 import glob
@@ -16,4 +16,4 @@ for path in paths:
     date=(str(map.meta["t_rec"])).replace("_00:00:00_TAI","")
     if (date not in dates):
         dates.append(date)
-[print(date) for date in dates]
+[print(date) for date in sorted(dates)]
