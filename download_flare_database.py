2150003f508f3582806fbb8e4485f58dd7f6b087
Akito <akitrb001@gmail.com>
2020-06-08 18:54:56 +0900

    fix download_flare_database.py

--- download_flare_database.py
+++ download_flare_database.py
@@ -1,8 +1,9 @@
 import subprocess
 i=0
 while(1):
-    command ="python3 scrape_flare_database.py" + str(i)
-    if(subprocess.call(command)):
+    command =["python3","scrape_flare_database.py",str(i)]
+    print(command)
+    if(subprocess.call(command)==0):
         i+=1
         continue
     else:
