a606e20cc5bfbe9d19b106969f6d2cadcaf6be03
Akito Komatsu <akitrb001@gmail.com>
2020-06-12 22:57:19 +0900

    Make csvfile contains only M-class and X-class flare.

--- make_flare_history.py
+++ make_flare_history.py
@@ -1,8 +1,20 @@
 import time
 import pandas as pd
-flare_database_name = "flare_database/flare_database0.csv"
+import numpy as np
+from tqdm import tqdm
+flare_database_name = "Huge_Flare_database.csv"
 flare_df =pd.read_csv(flare_database_name)
 flare_df = flare_df.drop_duplicates()
 flare_df["Start"]=pd.to_datetime(flare_df["Start"])
-print(flare_df["Start"])
-with 
+flare_df["Derived Position"] = flare_df["Derived Position"].str.replace(" ","")
+# print(flare_df["Start"])
+def initialize_coordinate (flare_df):
+    flare_df["flare_24h"] = 0
+    flare_df["longitude"] = 0
+    for i in tqdm(range (len(flare_df))):
+        if(flare_df["GOES Class"][i][0]=="A" or flare_df["GOES Class"][i][0]=="B" or flare_df["GOES Class"][i][0]=="C"):
+
+            flare_df=flare_df.drop(i,axis=0)
+    flare_df.to_csv("Huge_Flare_database.csv")        
+initialize_coordinate(flare_df)
+
